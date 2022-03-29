05f351abef88a257cabb4d7a5c3030f60715e845
Rasujon3 <rasujon3@gmail.com>
2022-03-30 00:14:25 +0600

    ls get,show,dlt done

--- src/Shop/Shop.js
+++ src/Shop/Shop.js
@@ -1,6 +1,11 @@
 import React, { useState, useEffect } from "react";
 import Cart from "../Cart/Cart";
 import Product from "../Product/Product";
+import {
+  addToLocalStorage,
+  clearLocalStorage,
+  getFromLocalStorage,
+} from "../Utils/Utils";
 import "./Shop.css";
 
 const Shop = () => {
@@ -12,6 +17,26 @@ const Shop = () => {
       .then((res) => res.json())
       .then((data) => setProducts(data));
   }, []);
+
+  useEffect(() => {
+    if (products.length) {
+      const storedProductsIds = getFromLocalStorage();
+      console.log(storedProductsIds);
+      const previousCart = [];
+
+      for (const id in storedProductsIds) {
+        // console.log(id);
+        const foundProduct = products.find((product) => product.id == id);
+        // console.log(foundProduct);
+        if (foundProduct) {
+          const quantity = storedProductsIds[id];
+          foundProduct.quantity = quantity;
+          previousCart.push(foundProduct);
+        }
+      }
+      setCart(previousCart);
+    }
+  }, [products]);
 
   const handleAddToCart = (selectedProduct) => {
     let newCart = [];
@@ -25,11 +50,13 @@ const Shop = () => {
       selectedProduct.quantity = selectedProduct.quantity + 1;
       newCart = [...rest, selectedProduct];
     }
+    addToLocalStorage(selectedProduct.id);
     setCart(newCart);
   };
 
   const handleClearCart = () => {
     setCart([]);
+    clearLocalStorage();
   };
 
   return (
